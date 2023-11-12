19556cb5f57c06ff92afc9de01a0b7f7261d9b57
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-11-12 18:51:06 +0300

    Update DataPage.js

--- src/DataPage.js
+++ src/DataPage.js
@@ -1,21 +1,30 @@
 import React from "react";
-import "./styles.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import "./DataPage.css";
 
-function DataPage({ timerData }) {
+function DataPage() {
+  const location = useLocation();
+  const timerData = location.state?.timerData || [];
+
   return (
     <div className="DataPage">
       <h2>Timer Data</h2>
       <table>
         <thead>
           <tr>
-            <th>Time (seconds)</th>
+            <th>Time (Minutes)</th>
+            <th>Duration (seconds)</th>
+            <th>Details</th>
+            <th>Color</th>
           </tr>
         </thead>
         <tbody>
           {timerData.map((data, index) => (
             <tr key={index}>
-              <td>{data}</td>
+              <td>{data.time}</td>
+              <td>{data.duration}</td>
+              <td>{data.details}</td>
+              <td>{data.color}</td>
             </tr>
           ))}
         </tbody>
@@ -23,4 +32,5 @@ function DataPage({ timerData }) {
     </div>
   );
 }
+
 export default DataPage;
